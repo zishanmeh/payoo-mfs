b87eed70648cd7f360297c20c2ccc0876832152a
zishanmeh <zishanmehedihasan2@gmail.com>
2024-09-18 17:31:33 +0100

    Added functionality for cash out

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,19 @@
 document.getElementById("open-add-money").addEventListener("click", () => {
   const addMoneyForm = document.getElementById("add-money-form");
   addMoneyForm.classList.remove("hidden");
+
+  //   cash out form will hidden
+  const cashOutForm = document.getElementById("cash-out-money-form");
+  cashOutForm.classList.add("hidden");
+});
+
+document.getElementById("open-cash-out-money").addEventListener("click", () => {
+  const cashOutForm = document.getElementById("cash-out-money-form");
+  cashOutForm.classList.remove("hidden");
+
+  //   Add money form will be hidden
+  const addMoneyForm = document.getElementById("add-money-form");
+  addMoneyForm.classList.add("hidden");
 });
 
 document.getElementById("add-money").addEventListener("click", (event) => {
@@ -20,5 +33,25 @@ document.getElementById("add-money").addEventListener("click", (event) => {
     addMoneyForm.classList.add("hidden");
   } else {
     alert("Failed to add money");
+  }
+});
+
+document.getElementById("cash-out-money").addEventListener("click", (event) => {
+  event.preventDefault();
+  let amount = document.getElementById("money-amount").value;
+  let pin = document.getElementById("pin-number").value;
+  if (pin === "6227") {
+    const availableBalance = document.getElementById("available-balance");
+    const outMoneyNumber = parseFloat(amount);
+    const balanceNumber = parseFloat(availableBalance.innerText);
+    const newBalance = balanceNumber - outMoneyNumber;
+    availableBalance.innerText = newBalance;
+    document.getElementById("money-amount").value = "";
+    document.getElementById("pin-number").value = "";
+    //   cash out form will hidden
+    const cashOutForm = document.getElementById("cash-out-money-form");
+    cashOutForm.classList.add("hidden");
+  } else {
+    alert("Faield to cash out");
   }
 });
