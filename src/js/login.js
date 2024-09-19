3b4534c13f24df6c1dc07f256b413cf320248a5e
zishanmeh <zishanmehedihasan2@gmail.com>
2024-09-19 20:41:00 +0100

    Simple changed

--- src/js/login.js
+++ src/js/login.js
@@ -2,7 +2,7 @@ document.getElementById("login-btn").addEventListener("click", (event) => {
   event.preventDefault();
   const phonenumber = document.getElementById("phone-input").value;
   const pinNumber = document.getElementById("password-input").value;
-  if (phonenumber === "7502604963" && pinNumber === "6227") {
+  if (phonenumber === "12345678" && pinNumber === "6227") {
     window.location.href = "src/logged/home.html";
   } else {
     alert("Wrong password");
